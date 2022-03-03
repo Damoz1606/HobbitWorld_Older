0c3f7aedf6e927a03740c54716b76e2548bc6c95
Damoz1606 <panchitodmun@gmail.com>
2022-03-03 10:52:25 -0500

    LevelOne

---
+++ src/scenes/LevelOne.ts
@@ -0,0 +1,11 @@
+import { Scene } from "phaser";
+
+export class LevelOne extends Scene {
+    constructor() {
+        super({ key: "LevelOne" });
+    }
+
+    create(): void {
+        // this.scene.start("Game");
+    }
+}
