f263485f9957047af04484e5ce313de9e9547a12
Damoz1606 <panchitodmun@gmail.com>
2022-03-03 10:51:30 -0500

    LoadingLevel

---
+++ src/scenes/LoadingLevel.ts
@@ -0,0 +1,33 @@
+import { Scene } from "phaser";
+
+export class LoadingLevel extends Scene {
+    constructor() {
+        super({ key: "LoadingLevel" });
+    }
+    
+    preload(): void {
+        // this.load.image("background", "assets/background.png");
+        // this.load.image("ground", "assets/ground.png");
+        // this.load.image("star", "assets/star.png");
+        // this.load.image("bomb", "assets/bomb.png");
+        // this.load.spritesheet("dude", "assets/dude.png", {
+        // frameWidth: 32,
+        // frameHeight: 48
+        // });
+        // this.load.spritesheet("baddie", "assets/baddie.png", {
+        // frameWidth: 32,
+        // frameHeight: 32
+        // });
+        // this.load.spritesheet("explosion", "assets/explosion.png", {
+        // frameWidth: 32,
+        // frameHeight: 32
+        // });
+        // this.load.audio("jump", "assets/jump.wav");
+        // this.load.audio("explosion", "assets/explosion.wav");
+        // this.load.audio("music", "assets/music.mp3");
+    }
+    
+    create(): void {
+        // this.scene.start("Game");
+    }
+}
