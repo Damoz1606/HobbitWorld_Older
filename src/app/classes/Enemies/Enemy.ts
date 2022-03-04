6735efd6add57ee2e2d8f8ecd9efccc434271d73
Damoz1606 <panchitodmun@gmail.com>
2022-03-04 13:10:07 -0500

    Enemy

---
+++ src/app/classes/Enemies/Enemy.ts
@@ -0,0 +1,26 @@
+import { Character } from "../Character";
+import { Player } from "../Player/Player";
+
+export abstract class Enemy extends Character {
+    // private target: Player;
+
+    constructor(
+        scene: Phaser.Scene, 
+        x: number, 
+        y: number, 
+        texture: string, 
+        private target: Player,
+        frame?: string | number) {
+        super(scene, x, y, texture, frame, 30);
+    }
+
+    protected abstract preUpdate(): void;
+
+    public setTarget(target: Player): void {
+        this.target = target;
+    }
+
+    public getTarget(): Player {
+        return this.target;
+    }
+}
