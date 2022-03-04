816e972c23e7e4a7df8767e60f27448b699a0ec8
Damoz1606 <panchitodmun@gmail.com>
2022-03-04 15:54:41 -0500

    Load assets

--- src/app/scenes/LoadingLevel.ts
+++ src/app/scenes/LoadingLevel.ts
@@ -8,7 +8,12 @@ export class LoadingLevel extends Scene {
     preload(): void {
         this.load.baseURL = 'assets/images/';
         this.load.atlas('hobbit', 'sprites/hobbit/Hobbit-0.png', 'sprites/hobbit/Hobbit.json');
+        this.load.atlas('crying-bud', 'sprites/enemy/CryingBud.png', 'sprites/enemy/CryingBud.json');
+        this.load.atlas('crying-water-bud', 'sprites/enemy/CryingWaterBud.png', 'sprites/enemy/CryingWaterBud.json');
         this.load.atlas('minor-minion', 'sprites/enemy/minorMinion.png', 'sprites/enemy/minorMinion.json');
+        this.load.atlas('phantom', 'sprites/enemy/Phatom.png', 'sprites/enemy/Phatom.json');
+        this.load.atlas('slime-brown', 'sprites/enemy/SlimeBrown.png', 'sprites/enemy/SlimeBrown.json');
+        this.load.atlas('slime-green', 'sprites/enemy/SlimeGreen.png', 'sprites/enemy/SlimeGreen.json');
     }
     
     create(): void {
