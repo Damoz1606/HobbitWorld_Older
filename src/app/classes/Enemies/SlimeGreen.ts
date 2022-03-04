bdff0d1a25dd8cea0e4702846b478cd6cc78bd7b
Damoz1606 <panchitodmun@gmail.com>
2022-03-04 15:28:11 -0500

    SlimeGreen

---
+++ src/app/classes/Enemies/SlimeGreen.ts
@@ -0,0 +1,37 @@
+import { Player } from "../Player/Player";
+import { Enemy } from "./Enemy";
+
+export class SlimeGreen extends Enemy {
+
+    constructor(
+        scene: Phaser.Scene,
+        x: number,
+        y: number,
+        texture: string,
+        target: Player, frame?: string | number) {
+        super(scene, x, y, texture, target, frame);
+        this.scale = 2.25;
+    }
+
+    protected preUpdate(): void {
+        this.getBody().setVelocityX(200);
+    }
+
+    protected initAnimations(): void {
+        this.scene.anims.create({
+            key: 'walk',
+            frames: this.scene.anims.generateFrameNames('slime-green', {
+                prefix: 'tile0',
+                suffix: '.png',
+                start: 251,
+                end: 254
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+
+    public update(): void {
+        !this.anims.isPlaying && this.anims.play('walk', true);
+    }
+}
